43f784d9f8a62de06e7a7d700f3e22cd5d34a80f
Carlos Villanueva <ceva_19@hotmail.com>
2019-01-02 12:06:02 -0300

    Java Persistence Api 2.1 - Chapter 6 - Transaction Scoped

--- ch06/ch06-01-txScopedEntiyManager/src/main/java/com/ejb/beans/ProjectService.java
+++ ch06/ch06-01-txScopedEntiyManager/src/main/java/com/ejb/beans/ProjectService.java
@@ -23,6 +23,19 @@ public class ProjectService {
     
     /**
      * 1. Buscamos una instancia de Employee y Project
+     * 2. Cuando el primero metodo find() es invocado, el contenedor verifica si hay una transaccion por defecto, el contenedor
+     *    se asegurara que una transaccion este activa si un session bean metodo inicia, asi el entity manager en este ejemplo
+     *    encontrara una lista. Este luego, verifica por un contexto de persistencia. Esta es la primera vez que una llamada al 
+     *    entity manager ha ocurrido, asi que aun no hay un persistence context. El entity manager crea uno nuevo y lo utiliza
+     *    para encontrar el proyecto.
+     * 3. Cuando el entity manager es usado para buscar un empleado, este verifica la transaccion nuevamente y esta vez encuentra
+     *    una creada cuando se busco el project. Este entonces, reutiliza el contexto de persistencia para buscar el empleado.
+     *    Em este punto, empleado y proyecto ambos son entity managed.
+     * 4. El empleado es luego adicionado al proyecto, actualizando las entidades employee y project. Cuando la llamda al metodo
+     *    termina, la transaccion es committed. Ya que employee y project fueron administradas. el contexto de persistencia puede
+     *    detectar cualquier estado de cambio en ellos, y este actualiza la base de datos durante el commit. Cuando la transaccion
+     *    termina, el contexto de persistencia se va, este proceso se repite cada vez que una o mas operaciones entity manager
+     *    son invocados dentro de una transaccion
      * @param empId
      * @param projectId 
      */
