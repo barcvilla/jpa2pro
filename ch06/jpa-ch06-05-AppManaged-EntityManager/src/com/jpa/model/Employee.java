89a8dcbbe6b8f00237f1a2814ce9e6bd77fea2f4
Carlos Villanueva <ceva_19@hotmail.com>
2019-01-10 01:32:36 -0300

    JPA Pro 2 - Chapter 6 - EntityManager Application Managed

---
+++ ch06/jpa-ch06-05-AppManaged-EntityManager/src/com/jpa/model/Employee.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jpa.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author barcvilla
+ */
+@Entity
+@Table(name = "employee")
+public class Employee implements Serializable
+{
+    @Id
+    private int id;
+    private String name;
+    private double salary;
+    
+    public Employee()
+    {}
+    
+    public Employee(int id)
+    {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    
+    public String toString()
+    {
+        return "Employee id: " + getId() + " name: " + getName() + " salary: " + getSalary();
+    }
+}
