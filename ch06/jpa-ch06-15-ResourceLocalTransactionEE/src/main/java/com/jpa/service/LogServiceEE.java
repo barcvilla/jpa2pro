013310a01dcc5c08c2d4d70d4f54a7b6c35843ae
Carlos Villanueva <ceva_19@hotmail.com>
2019-02-27 14:37:11 -0300

    JPA PRO 2 - Chapter 6 - Persistence

---
+++ ch06/jpa-ch06-15-ResourceLocalTransactionEE/src/main/java/com/jpa/service/LogServiceEE.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jpa.service;
+
+import com.jpa.domain.LogRecord;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
+
+/**
+ *
+ * @author PC
+ */
+@Stateless
+public class LogServiceEE {
+    @PersistenceContext(unitName = "Logging")
+    EntityManager em;
+    
+    public void logAccess(int userId, String action)
+    {
+           LogRecord lr = new LogRecord(userId, action);
+           em.persist(lr);
+        
+    }
+}
