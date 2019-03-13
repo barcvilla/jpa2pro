05f277496899dcdb1ed35374655d7673be59e888
Carlos Villanueva <ceva_19@hotmail.com>
2019-03-13 18:23:15 -0300

    Java JPA 2 - Chapter 6 - EntityManager - Persistence Context - TransactionScoped

---
+++ ch06/jpa-ch06-15-6-TrxScope-PersistenceCtx/src/main/java/com/jpa/model/Employee.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jpa.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author PC
+ */
+@Entity
+@Table(name = "employee_em_txscope")
+public class Employee implements Serializable{
+    @Id
+    private int id;
+    private String name;
+    
+    @Temporal(TemporalType.DATE)
+    private Date startDate;
+    
+    private long salary;
+    
+    public Employee(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "id=" + id + ", name=" + name + ", startDate=" + startDate + ", salary=" + salary + '}';
+    }
+    
+}
