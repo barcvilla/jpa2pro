9af0b2c66a3015eb8e0e74a21159429e88d5c0f9
Carlos Villanueva <ceva_19@hotmail.com>
2019-03-13 11:48:04 -0300

    Java JPA 2 - Chapter 6 - AppManaged EntityManager SE and EE

--- ch06/jpa-ch06-15-4-ApplicationM-EM-SE/src/main/java/com/jpa/view/EmployeeClient.java
+++ ch06/jpa-ch06-15-4-ApplicationM-EM-SE/src/main/java/com/jpa/view/EmployeeClient.java
@@ -8,6 +8,7 @@ package com.jpa.view;
 import com.jpa.model.Employee;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -17,6 +18,12 @@ import javax.persistence.Persistence;
  * el ciclo de vida de las entidades. 
  * Este es el unico tipo de entity manager disponible en Java SE.
  * Lo que separa Entity Manager Application Manager y Entity Manager Container Managed es como se crea el entity manager.
+ * La clase Persistence ofrece 2 variaciones del mismo metodo createEntityManager() puede ser usado para crear
+ * una instancia del EntityManagerFactory para una unidad de persistencia. Primero, especificar solo el nombre de la
+ * unidad de persistencia, retorna el factory con las propiedades por defecto definidad en el archivo persistence.xml
+ * La segunda forma de la llamada al metodo permite mapear propiedades para ser pasados,adicionar o sobreescribir las
+ * propiedades especificadas en el persistence.xml Esta forma es muy util cuando se requiere propiedades JDBC que no se
+ * pueden conocer hasta que la aplicacion sea instalada, 
  * @author PC
  */
 public class EmployeeClient {
@@ -24,13 +31,19 @@ public class EmployeeClient {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("EmployeeService");
         EntityManager em = emf.createEntityManager();
         
-        Collection emps = em.createQuery("SELECT e FROM Employee e").getResultList();
+        List<Employee> emps = em.createQuery("SELECT e FROM Employee e").getResultList();
+        for(Employee e : emps)
+        {
+            System.out.println("employee data: " + e.getId() + " " + e.getName());
+        }
+        /**
         for(Iterator i = emps.iterator(); i.hasNext();)
         {
             Employee e = (Employee)i.next();
             System.out.println("Employee: " + e.getId() + " " + e.getName());
         }
-        em.clear();
+        * **/
+        em.close();
         emf.close();
     }
 }
